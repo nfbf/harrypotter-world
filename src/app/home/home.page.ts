c9572430f7f69d9f56a26c901081bc7f1b322f0a
João Pedro Tavares <34795195+JohnPitter@users.noreply.github.com>
2020-11-21 22:13:14 -0300

    Adicionando a musica tema

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { NativeAudio } from '@ionic-native/native-audio/ngx';
 
 @Component({
   selector: 'app-home',
@@ -7,6 +8,17 @@ import { Component } from '@angular/core';
 })
 export class HomePage {
 
-  constructor() {}
+  constructor(private nativeAudio: NativeAudio) {
+    this.nativeAudio.preloadSimple('musicaTema', 'assets/music/hp-theme.mp3').then(this.onSucess, this.onError);
+    //this.nativeAudio.play('musicaTema').then(this.onSucess, this.onError);
+    //this.nativeAudio.loop('musicaTema').then(this.onSucess, this.onError);
+  }
 
+  onSucess(data){
+    console.log(data);
+  }
+
+  onError(data){
+    console.log(data);
+  }
 }
