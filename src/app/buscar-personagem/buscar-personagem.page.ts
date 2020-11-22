aecf73c610d5c4734f461ccfffcac3be82be7492
João Pedro Tavares <34795195+JohnPitter@users.noreply.github.com>
2020-11-21 23:58:52 -0300

    Implementação da Barra de Pesquisa de Personagens

--- src/app/buscar-personagem/buscar-personagem.page.ts
+++ src/app/buscar-personagem/buscar-personagem.page.ts
@@ -11,30 +11,41 @@ import { InfoPersonagensService } from '../servicos/info-personagens.service';
 export class BuscarPersonagemPage implements OnInit {
 
   listaPersonagens: Personagem[];
-  personagensSeleciondos : Personagem[];
+  personagensSelecionados : Personagem[];
+  autocomplete: { input: string; };
 
   constructor(private infos: InfoPersonagensService) {
     this.listaPersonagens = new Array();
-    this.personagensSeleciondos = new Array();
+    this.personagensSelecionados = new Array();
+    this.autocomplete = { input: '' };
 
     this.infos.carregarPersonagens().then((sucess => {
       this.listaPersonagens = sucess;
     }), (error => {
-      console.log('Deu erro irmão')
+      console.log(error)
     }))
   }
 
   ngOnInit() {
   }
 
-  onSearchTerm(ev: CustomEvent) {
+  pesquisaPeloTermo(ev: CustomEvent) {
     const val = ev.detail.value;
 
+    if(!val){
+      this.personagensSelecionados = [];
+      return;
+    }
+
     if (val && val.trim() !== '') {
-      this.personagensSeleciondos = this.listaPersonagens.filter(term => {
+      this.personagensSelecionados = this.listaPersonagens.filter(term => {
         return term.name.toLowerCase().indexOf(val.trim().toLowerCase()) > -1;
       });
     }
+  }
+
+  itemSelecionado(item){
+    console.log(item)
   }
 
 }
